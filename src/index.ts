e91e1bbadfe6da2570c8b492735c782ba27f1ebf
Ming <ben014335@gmail.com>
2024-09-03 21:45:55 +0800

    modify：修改為通用型
    不限定資料夾，並且做分頁處理

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const oneDayInMs = 24 * 60 * 60 * 1000; // 一天
 
 export const handler: Handler = async (event: any) => {
   try {
-    const totalDeletedObjects = await processAllFolders();
+    const totalDeletedObjects = await processAllObjects();
     return {
       statusCode: 200,
       body: JSON.stringify(`清理完成，總共刪除 ${totalDeletedObjects} 個檔案`),
@@ -27,71 +27,62 @@ export const handler: Handler = async (event: any) => {
 };
 
 /**
- * 處理指定的資料夾
- * @returns 刪除的檔案總數
+ * 處理 S3 存儲桶中的所有物件
+ * @returns 刪除的物件總數
  */
-async function processAllFolders(): Promise<number> {
-  const bucketName = process.env.AWS_S3_BUCKET as string;
-  // 處理 Common 和 PickupRequest 兩個資料夾
-  const folders = ["Common", "PickupRequest"];
-  let totalDeletedObjects = 0;
 
-  for (const folder of folders) {
-    totalDeletedObjects += await processFolderContents(bucketName, folder);
-  }
+async function processAllObjects(): Promise<number> {
+  const bucketName = process.env.AWS_S3_BUCKET as string;
+  let totalDeletedObjects = 0;
+  let continuationToken: string | undefined;
+
+  // 分頁處理
+  do {
+    // 列出 S3 存儲桶中所有物件
+    const listObjectsResponse = await s3Client.send(
+      new ListObjectsV2Command({
+        Bucket: bucketName,
+        ContinuationToken: continuationToken,
+      })
+    );
+
+    if (listObjectsResponse.Contents) {
+      const objectsToDelete = await Promise.all(
+        listObjectsResponse.Contents.map((obj) =>
+          processObject(bucketName, obj)
+        )
+      );
+
+      // 過濾出需要刪除的物件
+      const filteredObjectsToDelete = objectsToDelete.filter(
+        (obj) => obj !== null
+      ) as { Key: string }[];
+
+      if (filteredObjectsToDelete.length > 0) {
+        // 刪除符合條件的物件
+        await deleteObjects(bucketName, filteredObjectsToDelete);
+        totalDeletedObjects += filteredObjectsToDelete.length;
+        console.log(`已刪除 ${filteredObjectsToDelete.length} 個檔案`);
+      }
+    }
+    // 取得下一頁的標記
+    continuationToken = listObjectsResponse.NextContinuationToken;
+  } while (continuationToken);
 
   return totalDeletedObjects;
 }
 
 /**
- * 處理資料夾中所有檔案
- * @returns 刪除的檔案數量
- */
-async function processFolderContents(
-  bucketName: string,
-  folder: string
-): Promise<number> {
-  const listObjectsResponse = await s3Client.send(
-    new ListObjectsV2Command({
-      Bucket: bucketName,
-      Prefix: `${folder}/`, // 取得的資料夾前綴
-    })
-  );
-
-  if (!listObjectsResponse.Contents) {
-    return 0;
-  }
-
-  const objectsToDelete = await Promise.all(
-    listObjectsResponse.Contents.map((obj) =>
-      processObject(bucketName, folder, obj)
-    )
-  );
-
-  const filteredObjectsToDelete = objectsToDelete.filter(
-    (obj) => obj !== null
-  ) as { Key: string }[];
-
-  if (filteredObjectsToDelete.length > 0) {
-    await deleteObjects(bucketName, filteredObjectsToDelete);
-    console.log(
-      `已從 ${folder} 資料夾刪除 ${filteredObjectsToDelete.length} 個資料`
-    );
-  }
-
-  return filteredObjectsToDelete.length;
-}
-
-/**
- * 檢查是否應該要刪除
- * @returns 如果對象需要刪除，回傳 Key；否則回傳 null
+ * 處理單個物件，決定是否需要刪除
+ * @param bucketName S3 存儲桶名稱
+ * @param obj S3 物件
+ * @returns 如果物件需要刪除，返回包含 Key 的物件；否則返回 null
  */
 async function processObject(
   bucketName: string,
-  folder: string,
   obj: any
 ): Promise<{ Key: string } | null> {
-  // 取得S3所有資料
+  // 取的該物件資料
   const headObjectResponse = await s3Client.send(
     new HeadObjectCommand({
       Bucket: bucketName,
@@ -103,11 +94,11 @@ async function processObject(
   const uploadTime = new Date(headObjectResponse.LastModified!);
   const now = new Date();
 
+  // 檢查是否為臨時文件且超過一天
   if (isTemporary && now.getTime() - uploadTime.getTime() > oneDayInMs) {
-    // 暫時性文件，檢查是否超過一天
     return { Key: obj.Key! };
-  } else if (folder === "PickupRequest" && !isTemporary) {
-    // PickupRequest 中的非暫時性文件，檢查 expirationDate
+  } else if (obj.Key!.startsWith("PickupRequest/") && !isTemporary) {
+    // 對於 PickupRequest 資料夾中的非臨時文件，檢查是否過期
     const expirationDate = headObjectResponse.Metadata?.expirationDate;
     if (expirationDate && now > new Date(expirationDate)) {
       return { Key: obj.Key! };
@@ -117,6 +108,11 @@ async function processObject(
   return null;
 }
 
+/**
+ * 從 S3 存儲桶中刪除指定的物件
+ * @param bucketName S3 存儲桶名稱
+ * @param objects 要刪除的物件
+ */
 async function deleteObjects(
   bucketName: string,
   objects: { Key: string }[]
